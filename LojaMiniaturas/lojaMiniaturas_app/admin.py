ef67ccda4f8b9019329bb4a698c692416c238dfa
Myllena <mssilva.egq@uesc.br>
2024-04-10 21:00:50 -0300

    subindo banco de dados

--- LojaMiniaturas/lojaMiniaturas_app/admin.py
+++ LojaMiniaturas/lojaMiniaturas_app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from lojaMiniaturas_app.models import Produtos
+from lojaMiniaturas_app.models import *
 
-admin.site.register(Produtos)
+admin.site.register(Carro)
+admin.site.register(Boneca)
+admin.site.register(FunkoPop)
+
+
 
--- LojaMiniaturas/lojaMiniaturas_app/models.py
+++ LojaMiniaturas/lojaMiniaturas_app/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
-class Produtos (models.Model):
-    nome = models.CharField(max_length=200)
+class Carro (models.Model):
+    nome = models.CharField(max_length=50)
     preco = models.DecimalField(max_digits = 6, decimal_places = 2)
     descricao = models.TextField()
     codigo = models.IntegerField()
 
     def __str__ (self):
         return self.nome
+
+class Boneca (models.Model):
+    nome = models.CharField(max_length=50)
+    preco = models.DecimalField(max_digits = 6, decimal_places = 2)
+    descricao = models.TextField()
+    codigo = models.IntegerField()
+
+    def __str__ (self):
+        return self.nome
+
+class FunkoPop (models.Model):
+    nome = models.CharField(max_length=50)
+    preco = models.DecimalField(max_digits= 6, decimal_places= 2)
+    descricao = models.TextField()
+    codigo = models.IntegerField()
+    
+    def __str__ (self):
+        return self.nome
+
+
