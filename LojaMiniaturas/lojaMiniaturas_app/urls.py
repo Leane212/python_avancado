622fc35655f13d185f62c4010d3a4f6f4ff9e0da
Myllena <mssilva.egq@uesc.br>
2024-04-10 22:25:34 -0300

    criando views e urls

--- LojaMiniaturas/lojaMiniaturas_app/urls.py
+++ LojaMiniaturas/lojaMiniaturas_app/urls.py
@@ -2,5 +2,8 @@ from lojaMiniaturas_app import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home),
+    path('', views.home, name='home'),
+    path('carro/', views.carro, name='carro'),
+    path('boneca/', views.boneca, name='boneca'),
+    path('funkoPop/', views.funkoPop, name='funcoPop')
 ]
--- LojaMiniaturas/lojaMiniaturas_app/views.py
+++ LojaMiniaturas/lojaMiniaturas_app/views.py
@@ -1,6 +1,23 @@
+from multiprocessing import context
 from django.shortcuts import render
+from lojaMiniaturas_app.models import *
 
 
 def home(request):
     return render(request, 'base.html')
+
+def carro (request):
+    carros = Carro.objects.order_by('id')
+    context - {'carros': carros}
+    return render(request, 'index.html', context)
+
+def boneca (request):
+    boneca = Boneca.objects.order_by('id')
+    context - {'boneca': boneca}
+    return render(request, 'base.html', context)
+
+def funkoPop (request):
+    funcoPop = FunkoPop.objects.order_by('id')
+    context - {'funcopop': funkoPop}
+    return render(request, 'base.html', context)
 
