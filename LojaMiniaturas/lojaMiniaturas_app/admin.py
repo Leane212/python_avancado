df2fb62e431acbcdc5e73122e9ecb2ec1b12a310
Myllena <mssilva.egq@uesc.br>
2024-04-12 21:12:22 -0300

    subindo att do banco de dados

--- LojaMiniaturas/lojaMiniaturas_app/admin.py
+++ LojaMiniaturas/lojaMiniaturas_app/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from lojaMiniaturas_app.models import Produto
+from lojaMiniaturas_app.models import Imagem
+from lojaMiniaturas_app.models import Categoria
+from lojaMiniaturas_app.models import Marca
+from lojaMiniaturas_app.models import Desconto
+from lojaMiniaturas_app.models import Especificacao
 
 admin.site.register(Produto)
-
-
-
-
+admin.site.register(Imagem)
+admin.site.register(Categoria)
+admin.site.register(Marca)
+admin.site.register(Desconto)
+admin.site.register(Especificacao)
--- LojaMiniaturas/lojaMiniaturas_app/models.py
+++ LojaMiniaturas/lojaMiniaturas_app/models.py
@@ -5,11 +5,40 @@ class Produto (models.Model):
     preco = models.DecimalField(max_digits = 6, decimal_places = 2)
     descricao = models.TextField()
     codigo = models.IntegerField()
-    categoria = models.CharField(max_length = 20)
-    imagem = models.CharField(null=True, max_length=50)
+    video = models.TextField(null=True)
+    data_cadastro = models.DateField(null=True)
+    
+    def __str__ (self):
+        return self.nome
+    
+class Imagem (models.Model):
+    nome = models.CharField(max_length=50)
 
     def __str__ (self):
         return self.nome
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50)
+    
+    def __str__ (self):
+        return self.nome
 
+class Marca (models.Model):
+    nome = models.CharField(max_length=20)
+    
+    def __str__ (self):
+        return self.nome
+class Desconto (models.Model):
+    valor = models.DecimalField(max_digits = 6, decimal_places = 2)
+    
+    def __str__ (self):
+        return self.nome
 
+class Especificacao (models.Model):
+    aviso = models.TextField()
+    recomendacao = models.TextField()
+    restricao = models.TextField()
+    origem = models.TextField()
+    
+    def __str__ (self):
+        return self.aviso
